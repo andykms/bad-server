6910437b260a98818de586ac52e63b791fff4685
Andrey Isaev <andrey.isaev.54.ru@gmail.com>
2025-11-09 10:39:59 +0300

    fix: add guard for get all orders

--- backend/src/routes/order.ts
+++ backend/src/routes/order.ts
@@ -15,7 +15,7 @@ import { Role } from '../models/user'
 const orderRouter = Router()
 
 orderRouter.post('/', auth, validateOrderBody, createOrder)
-orderRouter.get('/all', getOrdersValidation, auth, getOrders)
+orderRouter.get('/all', getOrdersValidation, auth, roleGuardMiddleware(Role.Admin), getOrders)
 orderRouter.get('/all/me', getOrdersCurrentUserValidation, auth, getOrdersCurrentUser)
 orderRouter.get(
     '/:orderNumber',

4e7f36a22a089392ab4108154713104ae9be974b
Andrey Isaev <andrey.isaev.54.ru@gmail.com>
2025-11-09 18:41:50 +0300

    refactor: limit

--- backend/src/utils/cron-config.ts
+++ backend/src/utils/cron-config.ts
@@ -2,7 +2,7 @@ import { CronJob } from 'cron'
 import { cleanTemp } from './clean-temp'
 
 export const cleanTempJob = CronJob.from({
-    cronTime: '*/10 * * * *',
+    cronTime: '*/30 * * * *',
     onTick: async () => {
         await cleanTemp()
     },
